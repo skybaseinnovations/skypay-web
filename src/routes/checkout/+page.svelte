8df394bfc151f753aa630b5fd535f59d2cf9a02b
sonaam <sonaam@skybase.com.np>
2024-03-21 13:21:04 +0545

    Fix the design in checkout page

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -151,36 +151,80 @@
 
 <CheckoutMaster>
 	<div class="row d-flex justify-content-center">
-		<div class="col-md-6 card p-3 mt-4">
-			<!--{#if $selectedProvider}-->
-			<!--	<p>{$selectedProvider.provider_name}</p>-->
-			<!--{/if}-->
-			{#if !$activePayment || !$selectedProvider}
-				{#if $apiKey}
-					<ProviderList />
-				{/if}
+		<div class="d-flex justify-content-center pt-4">
+			<img src="logo.svg" alt="" width="150">
+		</div>
+		<div class="col-md-10 card p-4 mt-4">
+			<div class="row">
+				<div class="col-md-6 p-4">
+					<div class="d-flex gap-1 align-items-center">
+						<img src="/icons/arrow.svg" alt="" width="24">
+						<span>Back to Merchant</span>
+					</div>
+					<div class="pt-3">
+						<h6>Order Details</h6>
+						<div class="d-flex justify-content-between align-items-center card-01 p-3 mt-2">
+							<div class="d-flex flex-column ">
+								<h6 class="m-0">SaralNova</h6>
+								<label for="">Order #12455</label>
+							</div>
+							<div class="d-flex gap-1 align-items-end">
+								<span>Rs.</span>
+								<h3 class="m-0">789.00</h3>
+							</div>
+						</div>
+					</div>
+					<div class="pt-3">
+						<h6>Selected Payment</h6>
+						<div class="d-flex justify-content-between align-items-center card-01 p-3 mt-2">
+							<div class="d-flex gap-2 align-items-center">
+								<img src="qr.png" alt="" width="50" height="50" class="object-fit-contain">
+								<h5 class="m-0">SaralNova</h5>
+							</div>
+							<div class="payment-type d-flex gap-1 align-items-center">Manual / QR <img src="/icons/upside-arrow.svg"
+																																												 alt="" width="12"></div>
+						</div>
+					</div>
+					<div class="pt-5">
+						<button
+							class="btn btn-tertiary d-flex align-items-center justify-content-center gap-1 w-100"
+							on:click={() => initiate()}
+						>
+							Proceed to Pay
+						</button>
+					</div>
 
-
-			{/if}
-			<div class="d-flex flex-column pt-4 pb-3 gap-2">
-				<div>
-					<h6 class="m-0">My Payment Providers</h6>
 				</div>
+				<div class="col-md-6 border-left p-4">
+					<div class="">
+						{#if !$activePayment || !$selectedProvider}
+							{#if $apiKey}
+								<ProviderList />
+							{/if}
+						{/if}
+					</div>
+				</div>
+			</div>
+			{#if $selectedProvider}
+				<p>{$selectedProvider.provider_name}</p>
+			{/if}
+<!--  Payment Options-->
+			<div class="d-flex flex-column pt-4 pb-3 gap-2">
 				{#if $activePayment}
 					<div class="d-flex flex-column gap-2">
 						<p class="m-0 text-muted">{$activePayment.id}|{$activePayment.status}</p>
 						<div class='d-flex justify-content-between'>
 							<div>
-							{#if $activePayment && $selectedProvider}
+								{#if $activePayment && $selectedProvider}
 
-								<button
-									class="btn btn-primary d-flex align-items-center justify-content-center gap-1"
-									on:click={() => changeProvider()}
-								>
-									Change Provider
-								</button>
-							{/if}
-						</div>
+									<button
+										class="btn btn-primary d-flex align-items-center justify-content-center gap-1"
+										on:click={() => changeProvider()}
+									>
+										Change Provider
+									</button>
+								{/if}
+							</div>
 							<div class="d-flex gap-2 justify-content-end">
 								<button
 									class="btn btn-outline-primary d-flex align-items-center justify-content-center gap-1"
@@ -208,5 +252,92 @@
 			</div>
 		</div>
 
+
+
+<!--		QR Checkout Sections-->
+		<div class="col-md-10 card p-4 mt-4">
+			<div class="row">
+				<div class="col-md-6 p-4">
+					<div class="d-flex gap-1 align-items-center">
+						<img src="/icons/arrow.svg" alt="" width="24">
+						<span>Back to Merchant</span>
+					</div>
+					<div class="pt-3">
+						<h6>Order Details</h6>
+						<div class="d-flex justify-content-between align-items-center card-01 p-3 mt-2">
+							<div class="d-flex flex-column ">
+								<h6 class="m-0">SaralNova</h6>
+								<label for="">Order #12455</label>
+							</div>
+							<div class="d-flex gap-1 align-items-end">
+								<span>Rs.</span>
+								<h3 class="m-0">789.00</h3>
+							</div>
+						</div>
+					</div>
+					<div class="pt-3">
+						<h6>Selected Payment</h6>
+						<div class="d-flex justify-content-between align-items-center card-01 p-3 mt-2">
+							<div class="d-flex gap-2 align-items-center">
+								<img src="qr.png" alt="" width="50" height="50" class="object-fit-contain">
+								<h5 class="m-0">SaralNova</h5>
+							</div>
+							<div class="payment-type d-flex gap-1 align-items-center">Manual / QR <img src="/icons/upside-arrow.svg"
+																																												 alt="" width="12"></div>
+						</div>
+					</div>
+					<div class="pt-5">
+						<div class="info-payment d-flex align-items-center pb-2"><img alt="" src="icons/info.svg" width="16"><span>You will  be redirected to the  Khalti’s payment page</span></div>
+						<div class="pt-1">
+							<button
+								class="btn btn-tertiary d-flex align-items-center justify-content-center gap-1 w-100"
+							>
+								Mark as Paid <img alt="" src="icons/check.svg" width="18">
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 border-left p-4">
+					<div class="info-payment d-flex align-items-center justify-content-end pb-2"><img alt="" src="icons/info.svg" width="16"><span>See how it works?</span></div>
+					<label for="">Please send the amount to these details</label>
+					<div class=" card-01 p-3 mt-2">
+					<table class="personal-details-table">
+						<tr>
+							<td>Account Name </td>
+							<td> :  Sonaam Hitang <img src="icons/copy.svg" width="16" alt=""></td>
+						</tr>
+						<tr>
+							<td>Account Number </td>
+							<td> :  98234567890  <img src="icons/copy.svg" width="16" alt=""></td>
+						</tr>
+						<tr>
+							<td>Remarks </td>
+							<td> : 6763  <img src="icons/copy.svg" width="16" alt=""></td>
+						</tr>
+					</table>
+					</div>
+          <div>
+						<div class="d-flex gap-2 align-items-center justify-content-center pt-4">
+							<div style="border-top: 1px solid #e3e3e3;width: 30%;height: 100%">
+							</div>
+							<label for="">or Pay with following QR</label>
+							<div style="border-top: 1px solid #e3e3e3;width: 30%;height: 100%">
+							</div>
+						</div>
+						<div class="d-flex justify-content-center align-items-center pt-3">
+							<img src="qr.png" alt="" width="180" height="180" class="object-fit-contain">
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+
+<!--	Footer Links	-->
+		<div class="footer-links d-flex justify-content-center align-items-center gap-2 py-5">
+			<a href="#">Terms</a>
+			<a href="#">Policy</a>
+		</div>
 	</div>
 </CheckoutMaster>
--- src/routes/checkout/ProviderList.svelte
+++ src/routes/checkout/ProviderList.svelte
@@ -29,22 +29,26 @@
 </script>
 
 <div class="row d-flex align-items-center flex-column pt-4">
-	<h5 class="text-center">Choose Payment Provider</h5>
+	<h5 class="">Please select your payment method</h5>
 
-	<div class="">
+	<div class="d-flex gap-2">
 		{#each $providers as provider (provider.id)}
-			<label for="{provider.id}"
-						 class="d-flex justify-content-between dashed-border {$selectedProvider === provider.id ? 'active' : ''} py-3 px-4 mt-3"
-						 style="cursor: pointer;">
-				<div class="d-flex gap-2">
-					<img src="{provider.provider_logo_url}" width="24" height="24" class="object-fit-contain"
-							 alt="{provider.provider_name}">
-					<span>{provider.provider_name}</span>
-					<div class="badge badge-primary">{provider.mode}</div>
-				</div>
-				<input on:change={() => selectProvider(provider.id)} name="provider_id" type="radio"
-							 class="form-check-input" id="{provider.id}" value="{provider.id}">
-			</label>
+			<div class="position-relative">
+				<label for="{provider.id}"
+							 class="d-flex justify-content-between dashed-border {$selectedProvider === provider.id ? 'active' : ''}  mt-3 provider-label"
+							 style="cursor: pointer;">
+					<div class="d-flex flex-column justify-content-center text-center gap-1">
+						<img src="{provider.provider_logo_url}" width="40" height="40" class="object-fit-contain"
+								 alt="{provider.provider_name}">
+						<span>{provider.provider_name}</span>
+					</div>
+					<input on:change={() => selectProvider(provider.id)} name="provider_id" type="radio"
+								 class="form-check-input d-none" id="{provider.id}" value="{provider.id}">
+				</label>
+				<img src="icons/upside-arrow.svg" alt="" width="16" style="position: absolute;top: 1.2rem;right: 0.4rem">
+
+			</div>
+
 		{/each}
 	</div>
 
